b8f11b763cac23ed87bcc2eaf2a0e4eade5cdeff
Travis <developer.material@gmail.com>
2017-04-16 15:16:49 -0400

    funcitonal dynamic product pages

--- app.js
+++ app.js
@@ -35,10 +35,6 @@ app.get('/newsletter', function(req,res){
   res.render('newsletter');
 });
 
-app.get('/product', function(req,res){
-  res.render('product');
-});
-
 app.post('/newsletter', urlencodedParser, function(req, res){
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
@@ -64,4 +60,190 @@ app.post('/newsletter', urlencodedParser, function(req, res){
       console.log('Message %s sent: %s', info.messageId, info.response);
   });
   res.render('newsletter-success', {data: req.body});
+});
+
+//APPAREL//
+
+app.get('/product/shirt', function(req,res){
+  var data = {
+    title: 'Shirt',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$20.00',
+    image: '../app/img/utilities/shirt.jpeg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/hat', function(req,res){
+  var data = {
+    title: 'Hat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$12.00',
+    image: '../app/img/utilities/hat.jpeg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/jacket', function(req,res){
+  var data = {
+    title: 'Jacket',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$60.00',
+    image: '../app/img/utilities/jacket.jpeg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/pants', function(req,res){
+  var data = {
+    title: 'Pants',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$50.00',
+    image: '../app/img/utilities/pants.jpeg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/shoes', function(req,res){
+  var data = {
+    title: 'Shoes',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$40.00',
+    image: '../app/img/utilities/shoes.jpeg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/jacket2', function(req,res){
+  var data = {
+    title: 'Jacket2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$10.00',
+    image: '../app/img/utilities/jacket2.jpeg'
+  };
+  res.render('product', {data: data});
+});
+
+//CAMP//
+
+app.get('/product/blanket', function(req,res){
+  var data = {
+    title: 'Blanket',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$140.00',
+    image: '../app/img/utilities/blanket.jpg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/lantern', function(req,res){
+  var data = {
+    title: 'Lantern',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$90.00',
+    image: '../app/img/utilities/lantern.jpg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/survival', function(req,res){
+  var data = {
+    title: 'Tent',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$110.00',
+    image: '../app/img/utilities/survival.jpg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/axe', function(req,res){
+  var data = {
+    title: 'Axe',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$85.00',
+    image: '../app/img/utilities/axe.jpg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/tent', function(req,res){
+  var data = {
+    title: 'Tent',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$40.00',
+    image: '../app/img/utilities/tent.jpeg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/sleeping-bag', function(req,res){
+  var data = {
+    title: 'Sleeping Bag',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$160.00',
+    image: '../app/img/utilities/sleeping-bag.jpg'
+  };
+  res.render('product', {data: data});
+});
+
+//UTILITIES//
+
+app.get('/product/pack', function(req,res){
+  var data = {
+    title: 'Pack',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$75.00',
+    image: '../app/img/utilities/pack.jpg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/canteen', function(req,res){
+  var data = {
+    title: 'Canteen',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$25.00',
+    image: '../app/img/utilities/centeen.jpg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/flint', function(req,res){
+  var data = {
+    title: 'Flint',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$30.00',
+    image: '../app/img/utilities/flint.jpg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/knife', function(req,res){
+  var data = {
+    title: 'Knife',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$70.00',
+    image: '../app/img/utilities/knife.jpg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/hook', function(req,res){
+  var data = {
+    title: 'Hook',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$60.00',
+    image: '../app/img/utilities/hook.jpg'
+  };
+  res.render('product', {data: data});
+});
+
+app.get('/product/log', function(req,res){
+  var data = {
+    title: 'Adventure Log',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: '$9.00',
+    image: '../app/img/utilities/log.jpg'
+  };
+  res.render('product', {data: data});
 });
--- app/scripts/hike_data.js
+++ app/scripts/hike_data.js
@@ -1,3 +1,4 @@
+
 $('.shop-imgs ul li a').click(function(){
   //var what = this;
   $.ajax({
@@ -8,15 +9,12 @@ $('.shop-imgs ul li a').click(function(){
     success: function(data){
       //var dos = $(what).parent('li').index();
       $(data.ProductData.shirt).each(function(index, value){
-        $('.product-title').html(value.title);
-        console.log(value.title);
+        //$('.product-title').html(value.title);
       });
     }
   });
 });
-function hokay(){
-  $('pc1').css('display', 'none');
-}
+
 $('.shop-options ul li a').click(function(e){
   e.preventDefault();
   var that = this;
@@ -31,30 +29,30 @@ $('.shop-options ul li a').click(function(e){
       if( uno === 0 ) {
         $(data.ShopData.apparel).each(function(index, value){
           console.log(value.image1);
-          $('.shop-imgs').html('<img src="'+value.image1+'" />' +
-                               '<img src="'+value.image2+'" />' +
-                               '<img src="'+value.image3+'" />' +
-                               '<img src="'+value.image4+'" />' +
-                               '<img src="'+value.image5+'" />' +
-                               '<img src="'+value.image6+'" />' );
+          $('.shop-imgs').html('<a href="/product/shirt"><img src="'+value.image1+'" /></a>' +
+                               '<a href="/product/hat"><img src="'+value.image2+'" /></a>' +
+                               '<a href="/product/jacket"><img src="'+value.image3+'" /></a>' +
+                               '<a href="/product/pants"><img src="'+value.image4+'" /></a>' +
+                               '<a href="/product/shoes"><img src="'+value.image5+'" /></a>' +
+                               '<a href="/product/jacket2"><img src="'+value.image6+'" /></a>' );
         });
       } else if ( uno === 1 ) {
         $(data.ShopData.camp).each(function(index, value){
-          $('.shop-imgs').html('<img src="'+value.image7+'" />' +
-                               '<img src="'+value.image8+'" />' +
-                               '<img src="'+value.image9+'" />' +
-                               '<img src="'+value.image10+'" />' +
-                               '<img src="'+value.image11+'" />' +
-                               '<img src="'+value.image12+'" />' );
+          $('.shop-imgs').html('<a href="/product/blanket"><img src="'+value.image7+'" /></a>' +
+                               '<a href="/product/lantern"><img src="'+value.image8+'" /></a>' +
+                               '<a href="/product/survival"><img src="'+value.image9+'" /></a>' +
+                               '<a href="/product/axe"><img src="'+value.image10+'" /></a>' +
+                               '<a href="/product/tent"><img src="'+value.image11+'" /></a>' +
+                               '<a href="/product/sleeping-bag"><img src="'+value.image12+'" /></a>' );
         });
       } else if ( uno === 2 ) {
         $(data.ShopData.utilities).each(function(index, value){
-          $('.shop-imgs').html('<img src="'+value.image13+'" />' +
-                               '<img src="'+value.image14+'" />' +
-                               '<img src="'+value.image15+'" />' +
-                               '<img src="'+value.image16+'" />' +
-                               '<img src="'+value.image17+'" />' +
-                               '<img src="'+value.image18+'" />' );
+          $('.shop-imgs').html('<a href="/product/pack"><img src="'+value.image13+'" /></a>' +
+                               '<a href="/product/canteen"><img src="'+value.image14+'" /></a>' +
+                               '<a href="/product/flint"><img src="'+value.image15+'" /></a>' +
+                               '<a href="/product/knife"><img src="'+value.image16+'" /></a>' +
+                               '<a href="/product/hook"><img src="'+value.image17+'" /></a>' +
+                               '<a href="/product/log"><img src="'+value.image18+'" /></a>' );
         });
       } else if ( uno === 3 ) {
         $(data.ShopData.gadgets).each(function(index, value){
